55dfa3d09c2f3dd48d8662d09d31dde63dd40b84
Diogo Dutra <diogodutra@gmail.com>
2019-05-19 11:25:07 +0100

    Create unique database each demo run. Added more comments to make clear the capability of working with multiple connections at the same time.

--- Main.py
+++ Main.py
@@ -1,38 +1,39 @@
 import os
+import uuid
 from sql_easy import SqlEasy
 
-file_name = 'demo.db'
-is_existent = os.path.exists(file_name)
+unique_name = './data/demo_' + str(uuid.uuid4()) + '.db'
+file_name = unique_name
 
-db = SqlEasy(file_name)
+db = SqlEasy(file_name) # Same syntax to connect to an existent database
+db.create_table( 'Jobs'
+                , 'id', 'INTEGER PRIMARY KEY'
+                , 'profession', 'TEXT'
+                )
+db.add_row('Jobs', 'Politician')
+db.add_row('Jobs', 'Writer')
+db.add_row('Jobs', 'Actor/Actress')
 
-if (not is_existent):
-    print('Populating new database named', file_name)
+# Now we will use another connection to database in order
+# to show that it works well even with multiple connections
+# writing in the same file at the same time.
+db_other_connection = SqlEasy(file_name)
+db_other_connection.create_table( 'Celebrities'
+                , 'id', 'INTEGER PRIMARY KEY'
+                , 'fname', 'VARCHAR(20)'
+                , 'lname', 'VARCHAR(30)'
+                , 'gender', 'CHAR(1)'
+                , 'birth_date', 'DATE'
+                , 'job_id', 'INT'
+                )
+db_other_connection.add_row('Celebrities', 'William', 'Shakespeare', 'm', '1961-10-25', '2')
+db_other_connection.add_row('Celebrities', 'Frank',   'Schiller',    'm', '1955-08-17', '1')
+db_other_connection.add_row('Celebrities', 'Jane',    'Wall',        'f', '1989-03-14', '3')
+db_other_connection.del_rows('Celebrities', 'fname="Frank"')
 
-    db.create_table( 'Jobs'
-                    , 'id', 'INTEGER PRIMARY KEY'
-                    , 'profession', 'TEXT'
-                    )
-    db.add_row('Jobs', 'Politician')
-    db.add_row('Jobs', 'Writer')
-    db.add_row('Jobs', 'Actor/Actress')
-
-    db_other_connection = SqlEasy(file_name)
-
-    db_other_connection.create_table( 'Celebrities'
-                    , 'id', 'INTEGER PRIMARY KEY'
-                    , 'fname', 'VARCHAR(20)'
-                    , 'lname', 'VARCHAR(30)'
-                    , 'gender', 'CHAR(1)'
-                    , 'birth_date', 'DATE'
-                    , 'job_id', 'INT'
-                    )
-    db_other_connection.add_row('Celebrities', 'William', 'Shakespeare', 'm', '1961-10-25', '2')
-    db_other_connection.add_row('Celebrities', 'Frank',   'Schiller',    'm', '1955-08-17', '1')
-    db_other_connection.add_row('Celebrities', 'Jane',    'Wall',        'f', '1989-03-14', '3')
-
-    db_other_connection.del_rows('Celebrities', 'fname="Frank"')
-
+# Mind that now we will use the 1st connection to check if
+# it is capable of accessing the modifications performed
+# by the other 2nd connection
 table = db.join('Celebrities', 'Jobs', 'job_id', 'id'
                 , columns='fname, lname, gender, profession')
 
