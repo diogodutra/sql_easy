2a923a13d44857f07605820977e495400c896a55
Diogo Dutra <diogodutra@gmail.com>
2019-05-19 02:49:15 +0100

    Changed example to show that SqlEasy can load existent dabatase

--- Main.py
+++ Main.py
@@ -2,36 +2,35 @@ import os
 from sql_easy import SqlEasy
 
 file_name = 'company.db'
-try:
-    os.remove(file_name)
-except:
-    print('Creating database named', file_name)
-
+is_existent = os.path.exists(file_name)
 db = SqlEasy(file_name)
 
 print('SQLite version: ', db.version())
 
-db.create_table( 'Jobs'
-                , 'id', 'INTEGER PRIMARY KEY'
-                , 'profession', 'TEXT'
-                )
-db.add_row('Jobs', 'Politician')
-db.add_row('Jobs', 'Writer')
-db.add_row('Jobs', 'Actor/Actress')
+if (not is_existent):
+    print('Creating database named', file_name)
 
-db.create_table( 'Celebrities'
-                , 'id', 'INTEGER PRIMARY KEY'
-                , 'fname', 'VARCHAR(20)'
-                , 'lname', 'VARCHAR(30)'
-                , 'gender', 'CHAR(1)'
-                , 'birth_date', 'DATE'
-                , 'job_id', 'INT'
-                )
-db.add_row('Celebrities', 'William', 'Shakespeare', 'm', '1961-10-25', '2')
-db.add_row('Celebrities', 'Frank',   'Schiller',    'm', '1955-08-17', '1')
-db.add_row('Celebrities', 'Jane',    'Wall',        'f', '1989-03-14', '3')
+    db.create_table( 'Jobs'
+                    , 'id', 'INTEGER PRIMARY KEY'
+                    , 'profession', 'TEXT'
+                    )
+    db.add_row('Jobs', 'Politician')
+    db.add_row('Jobs', 'Writer')
+    db.add_row('Jobs', 'Actor/Actress')
 
-db.del_rows('Celebrities', 'fname="Frank"')
+    db.create_table( 'Celebrities'
+                    , 'id', 'INTEGER PRIMARY KEY'
+                    , 'fname', 'VARCHAR(20)'
+                    , 'lname', 'VARCHAR(30)'
+                    , 'gender', 'CHAR(1)'
+                    , 'birth_date', 'DATE'
+                    , 'job_id', 'INT'
+                    )
+    db.add_row('Celebrities', 'William', 'Shakespeare', 'm', '1961-10-25', '2')
+    db.add_row('Celebrities', 'Frank',   'Schiller',    'm', '1955-08-17', '1')
+    db.add_row('Celebrities', 'Jane',    'Wall',        'f', '1989-03-14', '3')
+
+    db.del_rows('Celebrities', 'fname="Frank"')
 
 table = db.join('Celebrities', 'Jobs', 'job_id', 'id'
                 , columns='fname, lname, gender, profession')
